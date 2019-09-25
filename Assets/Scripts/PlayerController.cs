dcf7c7496d0b4db0578b6b806206dd0cfaa99b33
Gavin Crews <gavin98@live.ca>
2019-09-25 17:41:37 -0400

    fishished head joint and starting raycasting

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -20,4 +20,18 @@ public class PlayerController : MonoBehaviour
             0, Input.GetAxis("Vertical"));
         characterController.SimpleMove(moveDirection * moveSpeed);
     }
+
+    private void FixedUpdate()
+    {
+        Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"),
+            0, Input.GetAxis("Vertical"));
+        if (moveDirection == Vector3.zero)
+        {
+
+        }
+        else
+        {
+            head.AddForce(transform.right * 150, ForceMode.Acceleration);
+        }
+    }
 }
