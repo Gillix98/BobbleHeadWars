0759185be010fbecd932b8a68d25d382bcd9dc2a
Gavin Crews <gavin.crews@student.sl.on.ca>
2019-09-30 11:30:05 -0400

    marine makes noise when hurt and camera shake

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,11 +5,17 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 50.0f;
-    private CharacterController characterController;
+    public float[] hitForce;
     public Rigidbody head;
     public LayerMask layerMask;
-    private Vector3 currentLookTarget = Vector3.zero;
     public Animator bodyAnimator;
+    public float timeBetweenHits = 2.5f;
+
+    private bool isHit = false;
+    private float timeSinceHit = 0;
+    private int hitNumber = -1;
+    private Vector3 currentLookTarget = Vector3.zero;
+    private CharacterController characterController;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,6 +28,15 @@ public class PlayerController : MonoBehaviour
         Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"),
             0, Input.GetAxis("Vertical"));
         characterController.SimpleMove(moveDirection * moveSpeed);
+        if (isHit)
+        {
+            timeSinceHit += Time.deltaTime;
+            if (timeSinceHit > timeBetweenHits)
+            {
+                isHit = false;
+                timeSinceHit = 0;
+            }
+        }
     }
 
     private void FixedUpdate()
@@ -58,5 +73,32 @@ public class PlayerController : MonoBehaviour
         // 3
         transform.rotation = Quaternion.Lerp(transform.rotation,
                                 rotation, Time.deltaTime * 10.0f);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Alien alien = other.gameObject.GetComponent<Alien>();
+        if (alien != null)
+        {
+            //1
+            if (!isHit)
+            {
+                hitNumber += 1;
+                //
+                CameraShake cameraShake = Camera.main.GetComponent<CameraShake>();
+                if (hitNumber < hitForce.Length)
+                {
+                    cameraShake.intensity = hitForce[hitNumber];
+                    cameraShake.Shake();
+                }
+                else
+                {
+                    // death todo
+                }
+                isHit = true;
+                SoundManager.Instance.PlayOneShot(SoundManager.Instance.hurt);
+            }
+            alien.Die();
+        }
     }
 }
